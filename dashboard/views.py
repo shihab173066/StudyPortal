921231eb33541a055fa50e9b9d7b0a392baffe9b
shihab173066 <shihab.hossainSH@gmail.com>
2025-03-04 19:34:07 +0600

    updated notes

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'dashboard/home.html')
+
+def notes(request):
+    return render(request, 'dashboard/notes.html')
