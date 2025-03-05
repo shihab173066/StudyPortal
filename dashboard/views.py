3077e754ef3bae008cc7c3def1f080c0ad1706b8
shihab173066 <shihab.hossainSH@gmail.com>
2025-03-05 19:31:41 +0600

    implemented youtube and integrated todo

--- dashboard/views.py
+++ dashboard/views.py
@@ -93,7 +93,31 @@ def youtube(request):
     if request.method == "POST":
         form = DashboardForm(request.POST)
         text = request.POST['text']
-        video = VideosSearch(text,limit=50)
+        video = VideosSearch(text,limit=10)
+        result_list = []
+        for i in video.result()['result']:
+            result_dict = {
+                'input': text,
+                'title':i['title'],
+                'duration':i['duration'],
+                'thumbnail':i['thumbnails'][0]['url'],
+                'channel':i['channel']['name'],
+                'link':i['link'],
+                'views':i['viewCount']['short'],
+                'published':i['publishedTime'],
+            }
+            desc = ''
+            if i['descriptionSnippet']:
+                for j in i['descriptionSnippet']:
+                    desc += j['text']
+            result_dict['description'] = desc
+            result_list.append(result_dict)
+            context = {
+                'form':form, 
+                'results':result_list, 
+            }
+        return render(request, "dashboard/youtube.html", context)
+
     else:
         form = DashboardForm()
 
