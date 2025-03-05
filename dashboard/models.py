5a926bc2d896772bc980d1fe79fec31071a6cb5c
shihab173066 <shihab.hossainSH@gmail.com>
2025-03-05 17:00:35 +0600

    homework table added

--- dashboard/models.py
+++ dashboard/models.py
@@ -16,3 +16,10 @@ class Notes(models.Model):
         verbose_name_plural = "notes"
 
     
+class Homework(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    due = models.DateTimeField()
+    is_finished = models.BooleanField(default=False)
